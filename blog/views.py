4c0af1acd792f6fbd261e32ab389f448189d8d72
Yurim Seo <dek1313@naver.com>
2019-07-10 17:36:53 +0900

    first commit

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+# Create your views here.
+
+def home(request):
+    blogs = Blog.objects
+    return render(request, 'blog/home.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog_detail})
