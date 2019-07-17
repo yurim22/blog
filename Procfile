a77ee523cc1194cb14c179937cd3d86436093736
Yurim Seo <dek1313@naver.com>
2019-07-17 16:55:54 +0900

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: guicorn secondproject.wsgi --log-file-
--- secondproject/settings.py
+++ secondproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,3 +135,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media') 
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
