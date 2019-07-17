8dda1a39a2529da2f73c6b13ac22f708a44e6a3d
Yurim Seo <dek1313@naver.com>
2019-07-17 16:47:27 +0900

    heroku deploy

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rc(p8i&9i9on%aa$n0yo_jbs42yhchrlbyep3n(9@jeck&k4k='
+# SECRET_KEY = 'rc(p8i&9i9on%aa$n0yo_jbs42yhchrlbyep3n(9@jeck&k4k='
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'rc(p8i&9i9on%aa$n0yo_jbs42yhchrlbyep3n(9@jeck&k4k=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
